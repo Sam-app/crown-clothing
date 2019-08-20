c0825297f8080ca179c624e148c54222e00db5d7
Samuel <test@local.co.uk>
2019-08-20 02:54:42 +0100

    Added Sign-in component with custom and reusable form input controls

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 import HomePage from "./components/pages/homepage/HomePage.componenet";
 import ShopPage from "./components/pages/shoppage/ShopPage.component";
+import SignInAndSignUpPage from "./components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
